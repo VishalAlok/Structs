e78ea551fd8d3195d33715e44f8960933840a29a
vishal.Alok <you@example.com>
2021-10-18 05:05:41 +0530

    1st

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coordinate point = new Coordinate(10, 20);
+            Console.WriteLine(point.x); //output: 10  
+            Console.WriteLine(point.y); //output: 20
+        }
+    }
+    struct Coordinate
+    {
+        public int x;
+        public int y;
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
